051b084bb2cda2f0ce52010d1ed25d80ef7a9b26
redcliphaloe <redcliphaloe@gmail.com>
2022-05-31 01:42:53 +0300

    refactor: implement rule 3.5 in html-and-css.md

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 // Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
-
 // Problem: User interaction does not provide the correct results.
 // Solution: Add interactivity so the user can manage daily tasks.
 // Break things down into smaller steps and take each step at a time.
-
-
 // Event handling, user interaction is what starts the code execution.
-
 var taskInput = document.getElementById('new-task'); // Add a new task.
 var addButton = document.getElementsByTagName('button')[0]; // first button
 // ul of #incomplete
 var incompleteTaskHolder = document.getElementById('incomplete');
 // completed
 var completedTasksHolder = document.getElementById('completed');
-
-
 // New task list item
 var createNewTaskElement = function(taskString) {
-
   var listItem = document.createElement('li');
   listItem.className = 'list-item';
-
   // input (checkbox)
   var checkBox = document.createElement('input'); // checkbx
   // label
@@ -30,34 +22,27 @@ var createNewTaskElement = function(taskString) {
   var editInput = document.createElement('input'); // text
   // button.edit
   var editButton = document.createElement('button'); // edit button
-
   // button.delete
   var deleteButton = document.createElement('button'); // delete button
   var deleteButtonImg = document.createElement('img'); // delete button image
-
   label.innerText = taskString;
   label.className = 'task';
-  label.classList.add('lbl');  
-
+  label.classList.add('lbl');
   // Each elements, needs appending
   checkBox.type = 'checkbox';
   checkBox.classList.add('inpt');
   editInput.type = 'text';
   editInput.className = 'task';
   editInput.classList.add('inpt');
-
   // innerText encodes special characters, HTML does not.
   editButton.innerText = 'Edit';
   editButton.className = 'edit';
-  editButton.classList.add('btn');  
-
+  editButton.classList.add('btn');
   deleteButton.className = 'delete';
-  deleteButton.classList.add('btn');  
+  deleteButton.classList.add('btn');
   deleteButtonImg.classList.add('remove');
   deleteButtonImg.src = './remove.svg';
   deleteButton.appendChild(deleteButtonImg);
-
-
   // and appending.
   listItem.appendChild(checkBox);
   listItem.appendChild(label);
@@ -66,39 +51,27 @@ var createNewTaskElement = function(taskString) {
   listItem.appendChild(deleteButton);
   return listItem;
 }
-
-
-
 var addTask = function() {
   console.log('Add Task...');
   // Create a new list item with the text from the #new-task:
   if (!taskInput.value) return;
   var listItem = createNewTaskElement(taskInput.value);
-
   // Append listItem to incompleteTaskHolder
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
-
   taskInput.value = '';
-
 }
-
 // Edit an existing task.
-
 var editTask = function() {
   console.log('Edit Task...');
   console.log('Change \'edit\' to \'save\'');
-
-
   var listItem = this.parentNode;
-
   var editInput = listItem.querySelector('input[type="text"]');
   var label = listItem.querySelector('label');
   var editBtn = listItem.querySelector('.edit');
   var containsClass = listItem.classList.contains('edit-mode');
   // If class of the parent is .edit-mode
   if (containsClass) {
-
     // switch to .edit-mode
     // label becomes the inputs value.
     label.innerText = editInput.value;
@@ -107,36 +80,25 @@ var editTask = function() {
     editInput.value = label.innerText;
     editBtn.innerText = 'Save';
   }
-
   // toggle .edit-mode on the parent.
   listItem.classList.toggle('edit-mode');
 };
-
-
 // Delete task.
 var deleteTask = function() {
   console.log('Delete Task...');
-
   var listItem = this.parentNode;
   var ul = listItem.parentNode;
   // Remove the parent list item from the ul.
   ul.removeChild(listItem);
-
 }
-
-
 // Mark task completed
 var taskCompleted = function() {
   console.log('Complete Task...');
-
   // Append the task list item to the #completed
   var listItem = this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
-
 }
-
-
 var taskIncomplete = function() {
   console.log('Incomplete Task...');
   // Mark task as incomplete.
@@ -146,30 +108,20 @@ var taskIncomplete = function() {
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 }
-
-
-
 var ajaxRequest = function() {
   console.log('AJAX Request');
 }
-
 // The glue to hold it all together.
-
-
 // Set the click handler to the addTask function.
 addButton.onclick = addTask;
 addButton.addEventListener('click', addTask);
 addButton.addEventListener('click', ajaxRequest);
-
-
 var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
   console.log('bind list item events');
   // select ListItems children
   var checkBox = taskListItem.querySelector('input[type="checkbox"]');
   var editButton = taskListItem.querySelector('button.edit');
   var deleteButton = taskListItem.querySelector('button.delete');
-
-
   // Bind editTask to edit button.
   editButton.onclick = editTask;
   // Bind deleteTask to delete button.
@@ -177,30 +129,18 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
   // Bind taskCompleted to checkBoxEventHandler.
   checkBox.onchange = checkBoxEventHandler;
 }
-
 // cycle over incompleteTaskHolder ul list items
 // for each list item
 for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
-
   // bind events to list items chldren(tasksCompleted)
   bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
-
-
-
-
 // cycle over completedTasksHolder ul list items
 for (var i = 0; i < completedTasksHolder.children.length; i++) {
   // bind events to list items chldren(tasksIncompleted)
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
-
-
-
-
 // Issues with usability don't get seen
 // until they are in front of a human tester.
-
 // prevent creation of empty tasks.
-
 // Change edit to save when you are in edit mode.
